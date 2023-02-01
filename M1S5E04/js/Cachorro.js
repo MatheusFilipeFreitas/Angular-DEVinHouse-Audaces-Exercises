54ea45cca1ff60787f1e3b0d0f8a9e9907b6d533
Math <mathffreitas@hotmail.com>
2023-02-01 11:44:15 -0300

    M1S5E04

---
+++ M1S5E04/js/Cachorro.js
@@ -0,0 +1,7 @@
+import Animal from "./Animal.js";
+
+export class Cachorro extends Animal {
+    constructor(nome, idade) {
+        super(nome, idade, 'latido');
+    }
+}
---
+++ M1S5E04/js/Gato.js
@@ -0,0 +1,7 @@
+import Animal from "./Animal.js";
+
+export class Gato extends Animal {
+    constructor(nome, idade) {
+        super(nome, idade, 'miado');
+    }
+}
