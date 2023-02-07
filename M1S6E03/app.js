b41142a4227e8cedb2cd104fd75557a6f1cfff0a
Math <mathffreitas@hotmail.com>
2023-02-07 11:23:13 -0300

    M1S6E03

---
+++ M1S6E03/app.js
@@ -0,0 +1,45 @@
+function aprovarDadosCadastrais(cliente) {
+  return new Promise((resolve, reject) => {
+    if (cliente.cpf === 11111111111) resolve(true);
+    else reject(false);
+  });
+}
+
+function aprovarValorSolicitado(cliente, valor) {
+  return new Promise((resolve, reject) => {
+    if (cliente.margem >= valor) resolve(true);
+    else reject(false);
+  });
+}
+
+const cliente = { cpf: 11111111111, nome: 'Calleri', margem: 1000.0 };
+const aprovacaoCadastro = aprovarDadosCadastrais(cliente);
+const aprovacaoValor = aprovarValorSolicitado(cliente, 300.0);
+
+aprovacaoCadastro
+  .then(() => {
+    console.log('APROVAÇÃO DADOS:');
+    console.log('APROVADO');
+  })
+  .catch(() => {
+    console.log('APROVAÇÃO DADOS:');
+    console.log('REPROVADO');
+  })
+  .finally(() => {
+    console.log('APROVAÇÃO DADOS:');
+    console.log('Fim do Processamento');
+  });
+
+aprovacaoValor
+  .then(() => {
+    console.log('APROVAÇÃO VALOR:');
+    console.log('APROVADO');
+  })
+  .catch(() => {
+    console.log('APROVAÇÃO VALOR:');
+    console.log('REPROVADO');
+  })
+  .finally(() => {
+    console.log('APROVAÇÃO VALOR:');
+    console.log('Fim do Processamento');
+  });
