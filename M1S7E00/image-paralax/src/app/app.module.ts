a73f0009ae530d267795f2cff6dd687e0e326396
Math <mathffreitas@hotmail.com>
2023-02-13 19:36:08 -0300

    M1S7E02

--- M1S7E00/image-paralax/src/app/app.module.ts
+++ M1S7E00/image-paralax/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleryComponent } from './_components/galery/galery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
