fd1b1d9970bf11a8be49d6ecb63dc5295cc082da
Math <mathffreitas@hotmail.com>
2023-02-26 00:44:28 -0300

    M1S8E07

--- M1S8E00/StarWars/src/app/components/card/card.component.ts
+++ M1S8E00/StarWars/src/app/components/card/card.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/app/services/character.service';
+import { Character } from './../../common/character';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  characters: Character[] = [];
+
+  ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  constructor(private charaterService: CharacterService) {
+
+  }
+
+  getCharacters() {
+    this.charaterService.getCharacters().subscribe((response) => {
+      this.characters = response;
+    });
+  }
 
 }
--- M1S8E00/StarWars/src/app/services/character.service.ts
+++ M1S8E00/StarWars/src/app/services/character.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_PATH } from 'src/environments/environment';
+import { Character } from '../common/character';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,24 @@ import { Injectable } from '@angular/core';
 export class CharacterService {
 
   constructor(private http: HttpClient) { }
+
+  getCharacters(): Observable<Character[]> {
+    return this.http.get<Character[]>(`${API_PATH}/characters`);
+  }
+
+  getCharacter(): Observable<Character> {
+    return this.http.get<Character>(`${API_PATH}/character`);
+  }
+
+  createCharacter(character: Character) {
+    this.http.post<Character>(`${API_PATH}/character`, JSON.stringify(character));
+  }
+
+  deleteCharacter(id: number) {
+    this.http.delete<Character>(`${API_PATH}/character/${id}`);
+  }
+
+  updateCharacter(id: number, character: Character) {
+    this.http.put<Character>(`${API_PATH}/character/${id}`, JSON.stringify(character));
+  }
 }
